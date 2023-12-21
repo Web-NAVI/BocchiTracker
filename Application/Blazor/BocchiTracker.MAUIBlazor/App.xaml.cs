1efe5e1ccc05844cbd2c30a308d2231910de6e6c
Rumanooooo <andihincu95@gmail.com>
2023-12-22 00:12:38 +0100

    Progress in config design (functionallity pending)
    Todo:
    - Description format buttons
    - Ticket tables

--- Application/Blazor/BocchiTracker.MAUIBlazor/App.xaml.cs
+++ Application/Blazor/BocchiTracker.MAUIBlazor/App.xaml.cs
@@ -8,5 +8,18 @@
 
             MainPage = new MainPage();
         }
+
+        protected override Window CreateWindow(IActivationState activationState)
+        {
+            var window = base.CreateWindow(activationState);
+
+            const int newWidth = 1100;
+            const int newHeight = 600;
+
+            window.Width = newWidth;
+            window.Height = newHeight;
+
+            return window;
+        }
     }
 }
